8339ae4ea87cbdbace6fa460edf09e9a4e112d4a
brpthomas <pthoang6@gmail.com>
2018-02-16 22:58:41 -0500

    hope

--- webpack.config.js
+++ webpack.config.js
@@ -23,4 +23,9 @@ module.exports = {
     historyApiFallback: true,
     contentBase: './'
   }
+  plugins: [
+ new webpack.DefinePlugin({
+ 'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+ }),
+ ]
 };
